83a0ca38382bfb508e41be4ef1c48689f0738b4b
pretty123 <820168613@qq.com>
2015-12-25 10:09:11 +0800

    christmas  happy!

--- frontend/views/plug/christmas.php
+++ frontend/views/plug/christmas.php
@@ -5,10 +5,49 @@ p{text-align:center;}
   <source src="mp3/Jingle Bells.mp3" type="audio/mp3">
 </audio>
 <div>
-
 <p>Jingle Bells歌曲简介</p>
 <p>Jingle Bells,中文名铃儿响叮当，估计是全世界最经典的一首圣诞歌曲了，其欢乐的旋律能轻松地让你的心也随之一起舞动。让我们一起来细细咀嚼这首脍炙人口的Jingle Bells，从Jingle Bells中英文歌词到Jingle Bells的历史，享受一场文化盛宴。</p>
 <p>Jingle Bells</p>
+
+
+<p>Jingle bell, jingle bell, jingle bell rock</p>
+<p>Jingle bells swing and jingle bells ring</p>
+<p>Snowing and blowing up bushels of fun</p>
+<p>Now the jingle hop has begun</p>
+<p>Jingle bell, jingle bell, jingle bell rock</p>
+<p>Jingle bells chime in jingle bell time</p>
+<p>Dancing and prancing in Jingle Bell Square</p>
+<p>In the frosty air</p>
+<p>What a bright time, it's the right time</p>
+<p>To rock the night away</p>
+<p>Jingle bell time is a swell time</p>
+<p>To go gliding in a one-horse sleigh</p>
+<p>Giddy-up jingle horse, pick up your feet</p>
+<p>Jingle around the clock</p>
+<p>Mix and a-mingle in the jingling feet</p>
+<p>That's the jingle bell rock</p>
+
+<p>Jingle bell, jingle bell, jingle bell rock</p>
+<p>Jingle bells chime in jingle bell time</p>
+<p>Dancing and prancing in Jingle Bell Square</p>
+<p>In the frosty air</p>
+
+<p>What a bright time, it's the right time</p>
+<p>To rock the night away</p>
+<p>Jingle bell time is a swell time</p>
+<p>To go gliding in a one-horse sleigh</p>
+<p>Giddy-up jingle horse, pick up your feet</p>
+<p>Jingle around the clock</p>
+<p>Mix and a-mingle in the jingling feet</p>
+<p>That's the jingle bell,</p>
+<p>That's the jingle bell,</p>
+<p>That's the jingle bell rock.</p>
+
+</div>
+
+<div>
+
+
 
 <p>Dashing through the snow, in a one-horse open sleigh.</p>
 <p>冲过大风雪,坐在雪橇上</p>
