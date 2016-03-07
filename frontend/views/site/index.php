fe0d3d8308670a2b998f7a40ced94e89a48daa05
pretty123 <820168613@qq.com>
2016-03-07 10:50:59 +0800

    哥哥

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,22 +8,30 @@ $this->title = 'My Yii Application';
 
     <div class="jumbotron">
         <h1>城南旧事</h1>
-
-        <p class="lead">小孩子永远是天真的，他们的世界如此纯净，如此美好。</p>
-
-        <p><a class="btn btn-lg btn-success" href="#"></a></p>
+        <p class="lead">小孩子永远是天真的，他们的世界如此纯净，如此美好。</p>      
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>我们看海去</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>　我们看海去!
+
+　　蓝色的大海上，
+
+　　扬着白色的帆。
+
+　　金红的太阳，
+
+　　从海上升起来，
+
+　　照到海面照到船头。
+
+　　我们看海去!
+
+　　我们看海去!</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
